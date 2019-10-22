f9eac5f1940fb49b92b009e32df41a6f5b702b05
phette23 <phette23@gmail.com>
2019-10-22 11:50:30 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+// you can pass any valid parameter for the search API route on the CLI
+// https://vault.cca.edu/apidocs.do#!/search/searchItems_get_0
+// e.g. node index --order=relevance --count=500 --status=draft --modifiedAfter=2019-09-01
+const qs = require("qs")
+const request = require("request")
+let defaults = { length: 50, count: 200, "q": "", order: "modified" }
+let options = require('rc')('contribution-count', defaults)
+
+let headers = {
+    'X-Authorization': 'access_token=' + options.token,
+    'Content-Type': 'application/json',
+}
+let items = []
+// log messages only when debug=true
+let debug = (msg) => {
+    if (options.debug) console.error(msg)
+}
+
+function getItems(start=0) {
+    if (items.length < options.count) {
+        debug(`Getting items ${items.length + 1} through ${items.length + options.length}...`)
+        let reqOptions = {
+            headers: headers,
+            url: `${options.root}/search?start=${start}&info=detail&${qs.stringify(options)}`,
+            json: true
+        }
+        request(reqOptions, (err, resp, data) => {
+            if (err) throw err
+            // the first time through, if our count is higher than the total
+            // of available items, reset the count to be that total
+            if (start === 0 && data.available < options.count) options.count = data.available
+            items = items.concat(data.results)
+            // recursively call this function until we're done
+            return getItems(items.length)
+        })
+    } else {
+        collectionCount(items)
+    }
+}
+
+function collectionCount(items) {
+    debug(`Found ${items.length} total items matching search...`)
+    debug('Counting up collection sums...')
+    // will start out as a { uuid: 1 } hash & end up as
+    // { uuid: { count: 1, name: Libraries } }
+    let counts = {}
+    items.map(i => i.collection.uuid).forEach(c => {
+        // collection doesn't exist in hash yet, initialize at 0
+        if (!counts[c]) counts[c] = 0
+        return ++counts[c]
+    })
+
+    // convert collection UUIDs to names
+    debug('Getting names for collection UUIDs...')
+    let reqOptions = {
+        headers: headers,
+        url: `${options.root}/collection/?privilege=VIEW_ITEM`,
+        json: true
+    }
+    let uuids = Object.keys(counts)
+    request(reqOptions, (err, resp, data) => {
+        if (err) throw err
+        uuids.forEach(uuid => {
+            counts[uuid] = {
+                count: counts[uuid],
+                name: data.results.find(coll => coll.uuid === uuid).name
+            }
+        })
+        // print "sorted hash" as best we can in JS
+        console.log("Collection\tContribution count")
+        uuids.sort((a, b) => {
+            return counts[b].count - counts[a].count
+        }).forEach(uuid => {
+           console.log(`${counts[uuid].name}\t${counts[uuid].count}`);
+        });
+    })
+}
+
+getItems()
---
+++ readme.md
@@ -0,0 +1,28 @@
+# VAULT Contribution Count
+
+This script takes a VAULT search and then shows, for the given query, what the breakdown of contributions across collections is. Examples:
+
+```sh
+>  # last 500 published items
+> node index --count=500
+> # first 500 items published in 2018
+> node index --count=500 --modifiedAfter=2018-01-01 --reverse
+> # how many items were published in the Illustration Program collection before 2019
+> node index --count=Infinity --modifiedBefore=2019-01-01 --collections=5e6a957b-80d4-4dee-9081-7186586fbbe5
+> # the most recent 200 items that match the freetext query "painting"
+> node index --q=painting
+```
+
+Any of the parameters you can pass to the openEQUELLA Search API route are accepted on the command line: https://vault.cca.edu/apidocs.do#!/search/searchItems_get_0
+
+The defaults are: `length` 50 (no reason to change this, it is the maximum), `count` 200, and `order` by date last modified. The `collections` parameter is a comma-separated list of collection UUIDs. There is also a `debug` parameter which, when set, causes a few diagnostic messages to print to stderr.
+
+## Setup
+
+If you have node you can simply `npm install` the dependencies.
+
+You need an OAuth access token with the ability to search all collections and see items in all states (draft, live, etc). You can either pass this token on the command line as `--token=${token}` or (much easier) add it to a .contribution-countrc config file. Any parameter passed on the command line may be added to the configuration file where it will be the default but can be overridden by CLI parameters.
+
+## LICENSE
+
+[ECL Version 2.0](https://opensource.org/licenses/ECL-2.0)
